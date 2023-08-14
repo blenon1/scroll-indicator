d75fb4811844445a76a4fcd06513460258c1c18e
blenon1 <williamblenon@gmail.com>
2023-08-15 01:15:07 +0200

    premier depot

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const scrollIndicator = document.querySelector(".scroll-indicator");
+const content = document.querySelector(".content");
+
+const observer = new IntersectionObserver(handleIntersect);
+
+observer.observe(content);
+
+function handleIntersect(entries) {
+    const el = entries[0];
+    if(el.isIntersecting) {
+        window.addEventListener("scroll", indicatorAnimation)
+    }else if(!el.isIntersecting) {
+        window.removeEventListener("scroll", indicatorAnimation)
+    }
+}
+
+function indicatorAnimation() {
+    if(window.scrollY > content.offsetTop) {
+        const percentage = ((window.scrollY - content.offsetTop) / content.scrollHeight * 100).toFixed(2);
+        scrollIndicator.style.transform = `scaleX(${(percentage * 100)})`
+    }
+}
